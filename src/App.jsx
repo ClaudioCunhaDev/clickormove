cc3ae76a2bae37c4e1c95abb8e3313c03251b55b
Cunha <cunhanik1@gmail.com>
2023-09-12 19:56:27 +0100

    clickormove

---
+++ src/App.jsx
@@ -0,0 +1,204 @@
+import { useState } from "react";
+
+function App() {
+  const [coordenadas, setCoordenadas] = useState([]);
+  const [coordenadasRemovidas, setCoordenadasRemovidas] = useState([]);
+  const [drag, setDrag] = useState(false);
+  const cores = [
+    "red",
+    "yellow",
+    "green",
+    "blue",
+    "indigo",
+    "purple",
+    "pink",
+    "lime",
+  ];
+  const [start, setStart] = useState(false);
+
+  const handleClick = (e) => {
+    console.log(e.clientX, e.clientY);
+    if (start) {
+      const newCoord = {
+        x: e.clientX,
+        y: e.clientY,
+      };
+
+      setCoordenadas((prv) => [...prv, newCoord]);
+    }
+  };
+
+  const handleRemove = (e) => {
+    e.stopPropagation();
+    const lastPos = coordenadas[coordenadas.length - 1];
+    setCoordenadas((prv) => prv.filter((ele) => ele !== lastPos));
+    if (lastPos) {
+      setCoordenadasRemovidas((prv) => [...prv, lastPos]);
+    }
+  };
+  const handleAdd = (e) => {
+    e.stopPropagation();
+    const lastPos = coordenadasRemovidas[coordenadasRemovidas.length - 1];
+    setCoordenadasRemovidas((prv) => prv.filter((ele) => ele !== lastPos));
+    if (coordenadasRemovidas.length !== 0) {
+      setCoordenadas((prv) => [...prv, lastPos]);
+    }
+  };
+
+  const handleStart = (e) => {
+    e.stopPropagation();
+    setCoordenadas([]);
+    setCoordenadasRemovidas([]);
+    setStart(false);
+  };
+
+  const handleDrag = (e) => {
+    e.stopPropagation();
+    setDrag((prv) => !prv);
+  };
+
+  return (
+    <>
+      {!drag ? (
+        <div onClick={handleClick} className="bg-gray-800 h-screen">
+          {start ? (
+            <div className="flex flex-wrap md:flex gap-3 justify-center">
+              <button
+                onClick={handleRemove}
+                type="button"
+                className={`flex justify-center items-center z-20 w-[5rem] md:w-[10rem] text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br hover:ring-4 hover:outline-none hover:ring-green-300 dark:hover:ring-green-800 shadow-lg shadow-green-500/50 dark:shadow-lg dark:shadow-green-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 transition-all duration-300`}
+              >
+                REMOVE LAST
+              </button>
+              <button
+                onClick={handleAdd}
+                type="button"
+                className={`flex justify-center items-center z-20 w-[5rem] md:w-[10rem] text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br hover:ring-4 hover:outline-none hover:ring-green-300 dark:hover:ring-green-800 shadow-lg shadow-green-500/50 dark:shadow-lg dark:shadow-green-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 transition-all duration-300`}
+              >
+                ADD LAST
+              </button>
+              <button
+                onClick={handleDrag}
+                type="button"
+                className={`flex justify-center items-center z-20 w-[5rem] md:w-[10rem] text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br hover:ring-4 hover:outline-none hover:ring-green-300 dark:hover:ring-green-800 shadow-lg shadow-green-500/50 dark:shadow-lg dark:shadow-green-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 transition-all duration-300`}
+              >
+                {drag ? "CLICK GAME" : "DRAG GAME"}
+              </button>
+              <button
+                onClick={() => setCoordenadas([])}
+                className={`flex justify-center items-center z-20 w-[5rem] md:w-[10rem] text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br  shadow-lg shadow-red-500/50 dark:shadow-lg dark:shadow-red-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 transition-all duration-300`}
+              >
+                CLEAN BOARD
+              </button>
+              <button
+                onClick={handleStart}
+                type="button"
+                className={`flex justify-center items-center z-20 w-[5rem] md:w-[10rem] text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br  shadow-lg shadow-red-500/50 dark:shadow-lg dark:shadow-red-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 transition-all duration-300`}
+              >
+                {coordenadas.length === 0 ? "CANCEL GAME" : "EXIT GAME"}
+              </button>
+            </div>
+          ) : (
+            <div className="flex gap-3 justify-center items-center h-screen">
+              <button
+                onClick={() => setStart(true)}
+                className={`flex justify-center items-center w-[5rem] md:w-[10rem] text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br hover:ring-4 hover:outline-none hover:ring-green-300 dark:hover:ring-green-800 shadow-lg shadow-green-500/50 dark:shadow-lg dark:shadow-green-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 transition-all duration-300`}
+              >
+                START
+              </button>
+            </div>
+          )}
+
+          {coordenadas?.map((ele, idx) => (
+            <button
+              key={idx}
+              style={{
+                top: ele.y + "px",
+                left: ele.x + "px",
+                position: "absolute",
+              }}
+              className={`flex justify-center items-center bg-gradient-to-r from-${
+                cores[Math.floor(Math.random() * cores.length)]
+              }-400 via-${
+                cores[Math.floor(Math.random() * cores.length)]
+              }-500 to-${
+                cores[Math.floor(Math.random() * cores.length)]
+              }-600 w-5 h-5 border-2 rounded-full`}
+            ></button>
+          ))}
+        </div>
+      ) : (
+        <div onTouchMove={handleClick} className="bg-gray-800 h-screen">
+          {start ? (
+            <div className="flex flex-wrap md:flex gap-3 justify-center">
+              <button
+                onClick={handleRemove}
+                type="button"
+                className={`flex justify-center items-center z-20 w-[5rem] md:w-[10rem] text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br hover:ring-4 hover:outline-none hover:ring-green-300 dark:hover:ring-green-800 shadow-lg shadow-green-500/50 dark:shadow-lg dark:shadow-green-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 transition-all duration-300`}
+              >
+                REMOVE LAST
+              </button>
+              <button
+                onClick={handleAdd}
+                type="button"
+                className={`flex justify-center items-center z-20 w-[5rem] md:w-[10rem] text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br hover:ring-4 hover:outline-none hover:ring-green-300 dark:hover:ring-green-800 shadow-lg shadow-green-500/50 dark:shadow-lg dark:shadow-green-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 transition-all duration-300`}
+              >
+                ADD LAST
+              </button>
+              <button
+                onClick={handleDrag}
+                type="button"
+                className={`flex justify-center items-center z-20 w-[5rem] md:w-[10rem] text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br hover:ring-4 hover:outline-none hover:ring-green-300 dark:hover:ring-green-800 shadow-lg shadow-green-500/50 dark:shadow-lg dark:shadow-green-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 transition-all duration-300`}
+              >
+                {drag ? "CLICK GAME" : "DRAG GAME"}
+              </button>
+
+              <button
+                onClick={() => setCoordenadas([])}
+                className={`flex justify-center items-center z-20 w-[5rem] md:w-[10rem] text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br  shadow-lg shadow-red-500/50 dark:shadow-lg dark:shadow-red-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 transition-all duration-300`}
+              >
+                CLEAN BOARD
+              </button>
+              <button
+                onClick={handleStart}
+                type="button"
+                className={`flex justify-center items-center z-20 w-[5rem] md:w-[10rem] text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br  shadow-lg shadow-red-500/50 dark:shadow-lg dark:shadow-red-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 transition-all duration-300`}
+              >
+                {coordenadas.length === 0 ? "CANCEL GAME" : "EXIT GAME"}
+              </button>
+            </div>
+          ) : (
+            <div className="flex gap-3 justify-center items-center h-screen">
+              <button
+                onClick={() => setStart(true)}
+                className={`flex justify-center items-center w-[5rem] md:w-[10rem] text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br hover:ring-4 hover:outline-none hover:ring-green-300 dark:hover:ring-green-800 shadow-lg shadow-green-500/50 dark:shadow-lg dark:shadow-green-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 transition-all duration-300`}
+              >
+                START
+              </button>
+            </div>
+          )}
+
+          {coordenadas?.map((ele, idx) => (
+            <button
+              key={idx}
+              style={{
+                top: ele.y + "px",
+                left: ele.x + "px",
+                position: "absolute",
+              }}
+              className={`bg-gradient-to-r from-${
+                cores[Math.floor(Math.random() * cores.length)]
+              }-400 via-${
+                cores[Math.floor(Math.random() * cores.length)]
+              }-500 to-${
+                cores[Math.floor(Math.random() * cores.length)]
+              }-600 w-5 h-5 border-2 rounded-full`}
+            ></button>
+          ))}
+        </div>
+      )}
+    </>
+  );
+}
+
+export default App;
